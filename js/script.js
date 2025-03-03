bc5e770896fe2d0b55e3ac45f45a55d626e846af
Sarahi <larryrf3@gmail.com>
2025-03-02 23:39:31 -0600

    modifique elementos de html

--- js/script.js
+++ js/script.js
@@ -1,5 +1,25 @@
 // document.querySelector(".menu-toggle").addEventListener("click", function () {
 //     document.querySelector(".nav").classList.toggle("nav--open");
 //   });
-  
+document.addEventListener("DOMContentLoaded", () => {
+    // Menú responsivo
+    const menuToggle = document.querySelector(".menu-toggle");
+    const navList = document.querySelector(".nav__list");
+
+    menuToggle.addEventListener("click", () => {
+        navList.classList.toggle("nav__list--active");
+    });
+
+    // Formulario de contacto
+    const contactForm = document.querySelector(".contact__form");
+    
+    if (contactForm) {
+        contactForm.addEventListener("submit", (e) => {
+            e.preventDefault();
+            alert("Mensaje enviado correctamente");
+            contactForm.reset();
+        });
+    }
+});
+
 
